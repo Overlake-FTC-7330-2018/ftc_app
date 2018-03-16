270444e1edc7fd6efb84fc8584a5aef712d5d43c
msimp19 <michaelsimpson2019@gmail.com>
2018-03-15 17:05:27 -0700

    Add config for changing the number of positions, as well as definition for those positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/systems/common/LinearSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/systems/common/LinearSystem.java
@@ -8,10 +8,25 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 public class LinearSystem extends System {
 
-    protected static final double TOP = 1;
-    protected static final double BOTTOM = 0;
+    protected static final double ABSOLUTE_MAX = 1.0;
+    protected static final double ABSOLUTE_MIN = 0.0;
+
+    protected int max;
+
+    /**
+     * An array of values between the absolute min and max of size TBD by subclass containing
+     * a list of positions in an order declared
+      */
+    protected double[] positions;
 
     public LinearSystem(OpMode opMode, String systemName) {
         super(opMode, systemName);
+
+        this.positions = new double[config.getInt("arraySize")];
+        this.max = positions.length - 1;
+
+        for (int i = 0; i < positions.length; i++) {
+            positions[i] = config.getDouble("position" + i);
+        }
     }
 }
